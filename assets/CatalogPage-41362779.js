d194be2f041f298c685749dc6bbc3b874ef86e7e
Slavyasanek <Slavyasanek@users.noreply.github.com>
2023-10-19 19:17:59 +0000

    deploy: dcc242768b113c730cd19a77a4c62853260b11d4

---
+++ assets/CatalogPage-41362779.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-45498db5.js";const o=()=>t.jsx("h1",{children:"Catalog"});export{o as default};
---
+++ assets/FavouritePage-571de93d.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-45498db5.js";const e=()=>r.jsx("h1",{children:"favourite"});export{e as default};
