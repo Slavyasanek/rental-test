dc87bd094c15a20d3f0d740c5cefa7bc2094bb5b
Slavyasanek <Slavyasanek@users.noreply.github.com>
2023-10-20 18:36:14 +0000

    deploy: da83b6a34454b688778bed72caed386b2857461e

---
+++ assets/Home-a91b8d70.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-ca11675b.js";const r=()=>e.jsx("h1",{children:"Home"});export{r as default};
--- assets/Home-ed6670bb.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-1c1ba289.js";const r=()=>e.jsx("h1",{children:"Home"});export{r as default};
