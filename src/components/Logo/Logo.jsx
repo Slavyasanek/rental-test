6244ccb0f1d30964833b322e91c13b62e5ddfdf5
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-10-20 14:29:12 +0300

    update logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,8 +1,10 @@
 import { LogoWrapper, LogoIcon } from "./Logo.styled";
-
+import logo from '../../assets/logo.png';
 
 export const Logo = () => {
     return (
-        <LogoWrapper to={'/'}><p>CarRent</p><LogoIcon/></LogoWrapper>
+        <LogoWrapper to={'/'}><p>CarRent</p>
+        <LogoIcon src={logo} width={'20px'} alt="logo"/>
+        </LogoWrapper>
     )
 };
